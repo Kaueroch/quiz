7dde85e70359c970caa28e226ee9b1e292cb9b98
Kauê Alcantara Rocha <118750523+Kaueroch@users.noreply.github.com>
2024-05-10 11:52:08 -0300

    Add files via upload

---
+++ quiz/questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Qual é o principal mosquito transmissor da dengue?",
+        answer: "a)Aedes aegypti",
+        options: [
+            "a)Aedes aegypti",
+            "b)Aedes albopictus",
+            "c)Ambos",
+            "d)n.d.a"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Como a pessoa reconhece o mosquito Aedes aegypti?",
+        answer: "a)Pelas manchas em suas pernas",
+        options: [
+            "a)Pelas manchas em suas pernas",
+            "b)Pela picada",
+            "c)Pelos sintomas gerais",
+            "d)Pela cor do mosquito"
+        ]
+    },
+    {
+        numb: 3,
+        question: "É possível distinguir a picada do Aedes aegypti com a de um mosquito comum?",
+        answer: "b)Não",
+        options: [
+            "a)Sim",
+            "b)Não",
+            "c)Sim, pela cor do mosquito",
+            "d)n.d.a"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Todo Aedes aegypti transmite dengue?",
+        answer: "b)Não",
+        options: [
+            "a)Sim",
+            "b)Não",
+            "c)Não somente dengue como HPV tambem",
+            "d)n.d.a"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Como se transmite?",
+        answer: "a)Pela fêmea infectada",
+        options: [
+            "a)Pela fêmea infectada",
+            "b)Pelo macho infectado",
+            "c)Todos os mosquitos",
+            "d)n.d.a"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Qual NÃO é um sintoma da dengue?",
+        answer: "c)Coceira",
+        options: [
+            "a)Cansaço",
+            "b)Náuseas",
+            "c)Coceira",
+            "d)Falta de Apetite",
+            "e)Diarréia"
+        ]
+    },
+    {
+        numb: 7,
+        question: "O mosquito apenas contamina pessoas durante o calor?",
+        answer: "b)Não",
+        options: [
+            "a)Sim",
+            "b)Não",
+            "c)Durante todas as estações",
+            "d)n.d.a"
+        ]
+    },
+    {
+        numb: 8,
+        question: "As pessoas que já tiveram dengue alguma vez podem desenvolver o tipo hemorrágico?",
+        answer: "a)Sim",
+        options: [
+            "a)Sim",
+            "b)Não",
+            "c)Sim, e podem desenvolver outras doenças tambem",
+            "d)n.d.a"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Todo mundo que é picado pelo mosquito Aedes aegypti fica doente?",
+        answer: "a)É preciso que o mosquito esteja infectado com o vírus da dengue",
+        options: [
+            "a)É preciso que o mosquito esteja infectado com o vírus da dengue",
+            "b)Sim, o mosquito nasce com o vírus da dengue",
+            "c)Não, todo mundo tem anticorpos contra a dengue",
+            "d)n.d.a"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Quais são medidas de prevenção para evitar o contágio?",
+        answer: "a)Eliminar água parada que pode vir a se tornar um possível criadouro",
+        options: [
+            "a)Eliminar água parada que pode vir a se tornar um possível criadouro",
+            "b)Misturar borra de café com água e borrifar nas plantas",
+            "c)Secar lugares com água parada",
+            "d)Usar velas de citronela "
+        ]
+    },
+];
